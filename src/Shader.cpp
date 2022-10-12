aaf97dad5f5db61e18bcc0f8dc89e631253c22a7
Uriel Federez <uafederez@gmail.com>
2022-10-12 12:12:56 +0800

    Small formatting changes

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -2,145 +2,145 @@
 #include <wx/wx.h>
 
 Shader::Shader(const std::string& vsPath, const std::string& fsPath):
-	m_vertSourcePath(vsPath),
-	m_fragSourcePath(fsPath)
+    m_vertSourcePath(vsPath),
+    m_fragSourcePath(fsPath)
 {
-	CompileAndLinkProgram();
-	m_isValid = true;
+    CompileAndLinkProgram();
+    m_isValid = true;
 }
 
 Shader::Shader(Shader&& shader) noexcept:
-	m_isValid  (std::move(shader.m_isValid)),
-	m_programId(std::move(shader.m_programId)),
-	m_vertSourcePath(std::move(shader.m_vertSourcePath)),
-	m_fragSourcePath(std::move(shader.m_fragSourcePath))
+    m_isValid  (std::move(shader.m_isValid)),
+    m_programId(std::move(shader.m_programId)),
+    m_vertSourcePath(std::move(shader.m_vertSourcePath)),
+    m_fragSourcePath(std::move(shader.m_fragSourcePath))
 {
-	shader.m_programId = 0;
-	shader.m_isValid   = false;
+    shader.m_programId = 0;
+    shader.m_isValid   = false;
 }
 
 Shader& Shader::operator=(Shader&& shader) noexcept
 {
-	m_isValid		 = std::move(shader.m_isValid);
-	m_programId	     = std::move(shader.m_programId);
-	m_vertSourcePath = std::move(shader.m_vertSourcePath);
-	m_fragSourcePath = std::move(shader.m_fragSourcePath);
+    m_isValid        = std::move(shader.m_isValid);
+    m_programId      = std::move(shader.m_programId);
+    m_vertSourcePath = std::move(shader.m_vertSourcePath);
+    m_fragSourcePath = std::move(shader.m_fragSourcePath);
 
-	shader.m_isValid   = false;
-	shader.m_programId = 0;
+    shader.m_isValid   = false;
+    shader.m_programId = 0;
 
-	return *this;
+    return *this;
 }
 
 void Shader::UseProgram() {
-	glUseProgram(m_programId);
+    glUseProgram(m_programId);
 }
 
 void Shader::SetMatrix4x4(const char* uniformName, const glm::mat4& mat)
 {
-	glUniformMatrix4fv(glGetUniformLocation(m_programId, uniformName), 1, GL_FALSE, glm::value_ptr(mat));
+    glUniformMatrix4fv(glGetUniformLocation(m_programId, uniformName), 1, GL_FALSE, glm::value_ptr(mat));
 }
 
 void Shader::SetVector3(const char* uniformName, const glm::vec3& vec)
 {
-	glUniform3fv(glGetUniformLocation(m_programId, uniformName), 1, glm::value_ptr(vec));
+    glUniform3fv(glGetUniformLocation(m_programId, uniformName), 1, glm::value_ptr(vec));
 }
 
 void Shader::SetVector2(const char* uniformName, float f1, float f2)
 {
-	glUniform2f(glGetUniformLocation(m_programId, uniformName), f1, f2);
+    glUniform2f(glGetUniformLocation(m_programId, uniformName), f1, f2);
 }
 
 void Shader::SetFloat(const char* uniformName, const float f)
 {
-	glUniform1f(glGetUniformLocation(m_programId, uniformName), f);
+    glUniform1f(glGetUniformLocation(m_programId, uniformName), f);
 }
 
 void Shader::CompileAndLinkProgram()
 {
-	const std::string vertSource = LoadShaderSource(m_vertSourcePath);
-	const std::string fragSource = LoadShaderSource(m_fragSourcePath);
+    const std::string vertSource = LoadShaderSource(m_vertSourcePath);
+    const std::string fragSource = LoadShaderSource(m_fragSourcePath);
 
-	const char* vertSourceCStr = vertSource.c_str();
-	const char* fragSourceCStr = fragSource.c_str();
+    const char* vertSourceCStr = vertSource.c_str();
+    const char* fragSourceCStr = fragSource.c_str();
 
-	GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource (vertShader, 1, &vertSourceCStr, NULL);
-	glCompileShader(vertShader);
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource (vertShader, 1, &vertSourceCStr, NULL);
+    glCompileShader(vertShader);
 
-	if (!CheckIfCompileOrLinkSuccessful(vertShader, m_vertSourcePath))
-	{
-		std::cerr << "Vertex shader compilation failed!";
-		return;
-	}
+    if (!CheckIfCompileOrLinkSuccessful(vertShader, m_vertSourcePath))
+    {
+        std::cerr << "Vertex shader compilation failed!";
+        return;
+    }
 
-	GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource (fragShader, 1, &fragSourceCStr, NULL);
-	glCompileShader(fragShader);
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource (fragShader, 1, &fragSourceCStr, NULL);
+    glCompileShader(fragShader);
 
-	if (!CheckIfCompileOrLinkSuccessful(fragShader, m_fragSourcePath))
-	{
-		std::cerr << "Fragment shader compilation failed!";
-		return;
-	}
+    if (!CheckIfCompileOrLinkSuccessful(fragShader, m_fragSourcePath))
+    {
+        std::cerr << "Fragment shader compilation failed!";
+        return;
+    }
 
-	m_programId = glCreateProgram();
-	glAttachShader(m_programId, vertShader);
-	glAttachShader(m_programId, fragShader);
-	glLinkProgram(m_programId);
+    m_programId = glCreateProgram();
+    glAttachShader(m_programId, vertShader);
+    glAttachShader(m_programId, fragShader);
+    glLinkProgram(m_programId);
 
-	glDeleteShader(vertShader);
-	glDeleteShader(fragShader);
+    glDeleteShader(vertShader);
+    glDeleteShader(fragShader);
 
-	if (!CheckIfCompileOrLinkSuccessful(m_programId))
-	{
-		std::cerr << "Program linking failed!";
-		return;
-	}
+    if (!CheckIfCompileOrLinkSuccessful(m_programId))
+    {
+        std::cerr << "Program linking failed!";
+        return;
+    }
 }
 
 
 bool Shader::CheckIfCompileOrLinkSuccessful(GLuint id, const std::string& shaderPath)
 {
-	int success;
-	char infoLog[512];
+    int success;
+    char infoLog[512];
 
-	if (shaderPath.empty())	// id is for a program
-	{
-		glGetProgramiv(id, GL_LINK_STATUS, &success);
-		if (!success)
-		{
-			glGetProgramInfoLog(id, 512, NULL, infoLog);
-			wxLogDebug("Program link: %s\n", infoLog);
-			return false;
-		}
-	}
-	else // id is for a shader
-	{
-		glGetShaderiv(id, GL_COMPILE_STATUS, &success);
-		if (!success)
-		{
-			glGetShaderInfoLog(id, 512, NULL, infoLog);
-			wxLogDebug("In \'%s\': %s\n", shaderPath.c_str(), infoLog);
-			return false;
-		}
-	}
-	return true;
+    if (shaderPath.empty())	// id is for a program
+    {
+        glGetProgramiv(id, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(id, 512, NULL, infoLog);
+            wxLogDebug("Program link: %s\n", infoLog);
+            return false;
+        }
+    }
+    else // id is for a shader
+    {
+        glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(id, 512, NULL, infoLog);
+            wxLogDebug("In \'%s\': %s\n", shaderPath.c_str(), infoLog);
+            return false;
+        }
+    }
+    return true;
 }
 
 std::string Shader::LoadShaderSource(const std::string& path)
 {
-	std::ifstream file(path);
-	std::ostringstream oss;
+    std::ifstream file(path);
+    std::ostringstream oss;
 
-	oss << file.rdbuf();
-	return oss.str();
+    oss << file.rdbuf();
+    return oss.str();
 }
 
 Shader::~Shader()
 {
-	if (m_isValid)
-	{
-		glDeleteProgram(m_programId);
-	}
+    if (m_isValid)
+    {
+        glDeleteProgram(m_programId);
+    }
 }
