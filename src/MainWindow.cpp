fb83fb70d5cd60bfd1ced2f26b3aabd5e0811f0e
Uriel Federez <uafederez@gmail.com>
2022-10-14 22:27:24 +0800

    Resize horizontal split functionality

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,6 +1,8 @@
 #include "MainWindow.h"
 #include "VolumeMetadataDialog.h"
 
+#include <wx/splitter.h>
+
 MainWindow::MainWindow(const wxString& title)
 	: wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(250, 150))
 {
@@ -22,59 +24,74 @@ MainWindow::MainWindow(const wxString& title)
 
 	InitializeVolumeModel();
 
-	wxBoxSizer* sagittalPanelSizer    = new wxBoxSizer(wxVERTICAL);
-	wxBoxSizer* sagittalControlsSizer = new wxBoxSizer(wxHORIZONTAL);
-	m_sagittalViewSlider = new wxSlider(this, wxID_ANY, 0, 0, 100);
-	m_sagittalView       = new VolumeViewCanvas2D(this, m_volumeModel, AnatomicalAxis::SAGITTAL);
 
-	sagittalControlsSizer->Add(new wxStaticText(this, wxID_ANY, "Sagittal"), 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, 5);
-	sagittalControlsSizer->Add(m_sagittalViewSlider, 1, wxEXPAND, 5);
-	m_sagittalViewSlider->Disable();
+	wxBoxSizer* rootSizer = new wxBoxSizer(wxVERTICAL);
+	wxSplitterWindow* viewsMainSplitter = new wxSplitterWindow(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxSP_LIVE_UPDATE);
 
-	sagittalPanelSizer->Add(sagittalControlsSizer, 0, wxEXPAND);
-	sagittalPanelSizer->Add(m_sagittalView, 1, wxEXPAND);
+	viewsMainSplitter->SetSashGravity(0.5);
+	viewsMainSplitter->SetMinimumPaneSize(20);
+	rootSizer->Add(viewsMainSplitter, 1, wxEXPAND, 0);
 
-	wxBoxSizer* coronalPanelSizer    = new wxBoxSizer(wxVERTICAL);
-	wxBoxSizer* coronalControlsSizer = new wxBoxSizer(wxHORIZONTAL);
-	m_coronalViewSlider = new wxSlider(this, wxID_ANY, 0, 0, 100);
-	m_coronalView       = new VolumeViewCanvas2D(this, m_volumeModel, AnatomicalAxis::CORONAL);
-
-	coronalControlsSizer->Add(new wxStaticText(this, wxID_ANY, "Coronal"), 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, 5);
-	coronalControlsSizer->Add(m_coronalViewSlider, 1, wxEXPAND, 5);
-	m_coronalViewSlider->Disable();
-
-	coronalPanelSizer->Add(coronalControlsSizer, 0, wxEXPAND);
-	coronalPanelSizer->Add(m_coronalView, 1, wxEXPAND);
-
-	wxBoxSizer* horizontalPanelSizer    = new wxBoxSizer(wxVERTICAL);
-	wxBoxSizer* horizontalControlsSizer = new wxBoxSizer(wxHORIZONTAL);
-	m_horizontalViewSlider = new wxSlider(this, wxID_ANY, 0, 0, 100);
-	m_horizontalView       = new VolumeViewCanvas2D(this, m_volumeModel, AnatomicalAxis::HORIZONTAL);
-
-	horizontalControlsSizer->Add(new wxStaticText(this, wxID_ANY, "Horizontal"), 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, 5);
-	horizontalControlsSizer->Add(m_horizontalViewSlider, 1, wxEXPAND, 5);
-	m_horizontalViewSlider->Disable();
-
-	horizontalPanelSizer->Add(horizontalControlsSizer, 0, wxEXPAND);
-	horizontalPanelSizer->Add(m_horizontalView, 1, wxEXPAND);
-
-	m_rootCanvas3d   = new VolumeViewCanvas3D(this, m_volumeModel);
-
-	wxBoxSizer* rootSizer = new wxBoxSizer(wxHORIZONTAL);
-	wxBoxSizer* viewContainerSizer = new wxBoxSizer(wxVERTICAL);
-	wxBoxSizer* bottomViewsSizer   = new wxBoxSizer(wxHORIZONTAL);
-
-	bottomViewsSizer->Add(sagittalPanelSizer, 1, wxEXPAND);
-	bottomViewsSizer->Add(coronalPanelSizer, 1, wxEXPAND);
-	bottomViewsSizer->Add(horizontalPanelSizer, 1, wxEXPAND);
+	// Top panel
+	wxPanel* topViewPanel = new wxPanel(viewsMainSplitter, wxID_ANY);
+	{
+		wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+		topViewPanel->SetSizer(sizer);
+		m_rootCanvas3d = new VolumeViewCanvas3D(topViewPanel, m_volumeModel);
+		sizer->Add(m_rootCanvas3d, 1, wxEXPAND, 0);
+	}
 	
-	viewContainerSizer->Add(m_rootCanvas3d, 1, wxEXPAND);
-	viewContainerSizer->Add(bottomViewsSizer, 1, wxEXPAND);
+	// Bottom views panel
+	wxPanel* bottomViewsPanel = new wxPanel(viewsMainSplitter, wxID_ANY);
+	{
+		wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+		bottomViewsPanel->SetSizer(sizer);
 
-	//rootSizer->Add(new wxPanel(this, wxID_ANY), 1, wxEXPAND);
-	rootSizer->Add(viewContainerSizer, 4, wxEXPAND);
+		wxBoxSizer* sagittalPanelSizer    = new wxBoxSizer(wxVERTICAL);
+		wxBoxSizer* sagittalControlsSizer = new wxBoxSizer(wxHORIZONTAL);
+		m_sagittalViewSlider = new wxSlider(bottomViewsPanel, wxID_ANY, 0, 0, 100);
+		m_sagittalView       = new VolumeViewCanvas2D(bottomViewsPanel, m_volumeModel, AnatomicalAxis::SAGITTAL);
+
+		sagittalControlsSizer->Add(new wxStaticText(bottomViewsPanel, wxID_ANY, "Sagittal"), 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, 5);
+		sagittalControlsSizer->Add(m_sagittalViewSlider, 1, wxEXPAND | wxBOTTOM, 0);
+		m_sagittalViewSlider->Disable();
+
+		sagittalPanelSizer->Add(sagittalControlsSizer, 0, wxEXPAND);
+		sagittalPanelSizer->Add(m_sagittalView, 1, wxEXPAND);
+
+		wxBoxSizer* coronalPanelSizer    = new wxBoxSizer(wxVERTICAL);
+		wxBoxSizer* coronalControlsSizer = new wxBoxSizer(wxHORIZONTAL);
+		m_coronalViewSlider = new wxSlider(bottomViewsPanel, wxID_ANY, 0, 0, 100);
+		m_coronalView       = new VolumeViewCanvas2D(bottomViewsPanel, m_volumeModel, AnatomicalAxis::CORONAL);
+
+		coronalControlsSizer->Add(new wxStaticText(bottomViewsPanel, wxID_ANY, "Coronal"), 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, 5);
+		coronalControlsSizer->Add(m_coronalViewSlider, 1, wxEXPAND | wxBOTTOM, 0);
+		m_coronalViewSlider->Disable();
+
+		coronalPanelSizer->Add(coronalControlsSizer, 0, wxEXPAND);
+		coronalPanelSizer->Add(m_coronalView, 1, wxEXPAND);
+
+		wxBoxSizer* horizontalPanelSizer    = new wxBoxSizer(wxVERTICAL);
+		wxBoxSizer* horizontalControlsSizer = new wxBoxSizer(wxHORIZONTAL);
+		m_horizontalViewSlider = new wxSlider(bottomViewsPanel, wxID_ANY, 0, 0, 100);
+		m_horizontalView       = new VolumeViewCanvas2D(bottomViewsPanel, m_volumeModel, AnatomicalAxis::HORIZONTAL);
+
+		horizontalControlsSizer->Add(new wxStaticText(bottomViewsPanel, wxID_ANY, "Horizontal"), 0, wxALIGN_CENTER_VERTICAL | wxLEFT | wxRIGHT, 5);
+		horizontalControlsSizer->Add(m_horizontalViewSlider, 1, wxEXPAND | wxBOTTOM, 0);
+		m_horizontalViewSlider->Disable();
+
+		horizontalPanelSizer->Add(horizontalControlsSizer, 0, wxEXPAND, 0);
+		horizontalPanelSizer->Add(m_horizontalView, 1, wxEXPAND, 0);
+
+		sizer->Add(sagittalPanelSizer, 1, wxEXPAND, 0);
+		sizer->Add(coronalPanelSizer, 1, wxEXPAND, 0);
+		sizer->Add(horizontalPanelSizer, 1, wxEXPAND, 0);
+	}
+
+	viewsMainSplitter->SplitHorizontally(topViewPanel, bottomViewsPanel);
 
 	SetSizerAndFit(rootSizer);
+	rootSizer->SetSizeHints(this);
 }
 
 void MainWindow::InitializeVolumeModel()
