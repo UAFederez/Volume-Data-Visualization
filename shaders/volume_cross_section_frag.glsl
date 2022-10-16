b0ece577b2722f358721c8e340b7f3fef11239f2
Uriel Federez <uafederez@gmail.com>
2022-10-16 23:01:59 +0800

    Min-max normalization

--- shaders/volume_cross_section_frag.glsl
+++ shaders/volume_cross_section_frag.glsl
@@ -6,10 +6,12 @@ out vec4 FragmentColor;
 uniform sampler3D texture3d;
 uniform mat3  uvPermutation;
 uniform float sliceOffset;
+uniform float maxValue;
+uniform float minValue;
 
 void main()
 {    
-    vec3  sampleCoord = uvPermutation * vec3(TextureCoords.x, TextureCoords.y, sliceOffset);
-
-    FragmentColor = vec4(texture(texture3d, sampleCoord).rgb, 1.0f);
+    vec3 sampleCoord = uvPermutation * vec3(TextureCoords.x, TextureCoords.y, sliceOffset);
+    vec3 fragColor   = (texture(texture3d, sampleCoord).rgb - vec3(minValue)) / (vec3(maxValue - minValue));
+    FragmentColor = vec4(fragColor, 1.0f);
 }
